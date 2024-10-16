2aa353541a80c740e5619c04624a96550a8db8d3
ARJUN CHANDRA DAS <arjun.das@kuiperz.io>
2024-10-16 12:19:24 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <>
+      <div className="bg-gray-100 h-screen w-screen">
+        <h1 className="text-3xl font-bold text-center py-5">Counter Application</h1>
+        <div className="max-w-md mx-auto mt-10 space-y-5">
+          <Counter />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.tsx
@@ -0,0 +1,5 @@
+function Counter() {
+  return <div className="bg-white p-4 rounded">Counter</div>;
+}
+
+export default Counter;
